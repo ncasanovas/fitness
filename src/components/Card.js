b724525165a2a67eb6fb225668654f82329e8687
ncasanovas <nataliasolcasanovas@gmail.com>
2019-09-24 07:48:06 -0300

    First Commit

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import exerciseImg from '../images/exercise.png';
+import './styles/Card.css';
+
+
+class Card extends React.Component {
+  render(){
+    return (
+      <div className="card mx-auto Fitness-Card">
+        <div className="card-body">
+          <div className="row center">
+            <div classsName="col-6">
+              <img src={exerciseImg} className="float-right" />
+            </div>
+            <div classsName="col-6 Fitness-Card-Info">
+              <h1>Techniques Guides</h1>
+              <p>Learn amazing street workout and calisthenics. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Card;
